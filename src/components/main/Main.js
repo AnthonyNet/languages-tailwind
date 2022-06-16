5450fd2ac32d15e1a435f4630dc9fd5bfcf78245
AnthonyZet <zejda.anthony@gmail.com>
2022-06-16 20:11:07 +0200

    tailwind const style

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -8,6 +8,7 @@ const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 const styles= {
   section: "responsiveSection flex flex-col justify-center items-center w-full min-h-[88vh] h-auto sm:h-[90%] border-red-300  border-t-2",
+  h1: "text-3xl sm:text-4xl text-center font-bold text-gray-700",
 }
 
 //The whole components is imported  into pages/Home
