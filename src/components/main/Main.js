35772deb1df85d4f565b002932784d22936deaac
AnthonyZet <zejda.anthony@gmail.com>
2022-06-24 20:17:43 +0200

    css

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -11,7 +11,7 @@ const styles= {
   h2: "text-3xl sm:text-4xl text-center font-bold text-gray-700",
   sectionDiv:"mainCard w-[300px] h-[420px] bg-transparent cursor-pointer group perspective",
   article: "preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000",
-
+  articleDiv: "border-2 w-full h-full",
 }
 
 //The whole components is imported  into pages/Home
@@ -46,7 +46,7 @@ const Main = () => {
       </h2>
       <div className={styles.sectionDiv}>
         <article className={styles.article}>
-          <div className="border-2 w-full h-full">
+          <div className={styles.articleDiv}>
             <img
               src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg4.wikia.nocookie.net%2F__cb20081229142345%2Ffallout%2Fimages%2F2%2F2a%2FFallout2front.jpg&f=1&nofb=1"
               className="w-full h-full"
